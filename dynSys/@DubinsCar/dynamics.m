03bccb7809e1196967aadb7153381ad087a30e96
Mo Chen <mochen72@berkeley.edu>
2016-09-03 00:13:08 -0700

    made genericHam and genericPartial independent of dynamical system subsystem specs

--- dynSys/@DubinsCar/dynamics.m
+++ dynSys/@DubinsCar/dynamics.m
@@ -1,4 +1,4 @@
-function dx = dynamics(obj, ~, x, u, ~, ~)
+function dx = dynamics(obj, ~, x, u, ~, dims)
 % Dynamics of the Dubins Car
 %    \dot{x}_1 = v * cos(x_3)
 %    \dot{x}_2 = v * sin(x_3)
@@ -7,12 +7,16 @@ function dx = dynamics(obj, ~, x, u, ~, ~)
 %
 % Mo Chen, 2016-06-08
 
+if nargin < 6
+  dims = 1:obj.nx;
+end
+
 if iscell(x)
-  dx = cell(obj.nx, 1);
+  dx = cell(length(dims), 1);
   
-  dx{1} = obj.speed * cos(x{3});
-  dx{2} = obj.speed * sin(x{3});
-  dx{3} = u;
+  for i = 1:length(dims)
+    dx{i} = dynamics_cell_helper(obj, x, u, dims, dims(i));
+  end
 else
   dx = zeros(obj.nx, 1);
   
@@ -20,6 +24,18 @@ else
   dx(2) = obj.speed * sin(x(3));
   dx(3) = u;
 end
+end
 
+function dx = dynamics_cell_helper(obj, x, u, dims, dim)
 
+switch dim
+  case 1
+    dx = obj.speed * cos(x{dims==3});
+  case 2
+    dx = obj.speed * sin(x{dims==3});
+  case 3
+    dx = u;
+  otherwise
+    error('Only dimension 1-3 are defined for dynamics of DubinsCar!')
+end
 end
