d9d9a5a9262175a7413494c2333a7da87db8de03
Sylvia Herbert <sylvia.herbert@berkeley.edu>
2016-09-12 13:06:57 -0700

    added Quad6D stuff

---
+++ dynSys/@Quad6D/Quad6D.m
@@ -0,0 +1,128 @@
+classdef Quad6D < DynSys
+  % Note: Since quad is a "handle class", we can pass on
+  % handles/pointers to other plane objects
+  % e.g. a.platoon.leader = b (passes b by reference, does not create a copy)
+  % Also see constructor
+  
+  properties
+    % control bounds
+    T1Max
+    T1Min
+    T2Max
+    T2Min
+    
+    m % mass
+    
+    grav %gravity
+    
+    transDrag %translational drag
+    
+    rotDrag %rotational drag
+
+    Iyy %moment of Inertia
+    
+    l %whatever l is
+    
+    dims %dimensions that are active
+    
+  end
+  
+  methods
+    function obj = Quad6D(x, T1Max, T1Min, T2Max, T2Min,...
+        m, grav, transDrag, rotDrag, Iyy, l)
+      % obj = Quad6D(x, T1Max, T1Min, T2Max, T2Min,...
+      %  m, grav, transDrag, rotDrag, Iyy, l)
+      %
+      % Constructor. Creates a quadcopter object with a unique ID,
+      % state x, and reachable set information reachInfo
+      %
+      % Dynamics:
+      %    insert these later
+      %
+      % Inputs:
+      %   x      - state: [xpos; xvel; ypos; yvel; psipos; psivel]
+      %   T1Max, T1Min, T2Max, T2Min - inputs
+      %   m - mass
+      %   grav - gravity
+      %   transDrag - translational Drag
+      %   rotDrag - rotaitonal Drag
+      %   Iyy - moment of inertia
+      %   l - ???
+      %
+      % Output:
+      %   obj       - a Quadcopter Object
+      %
+      % Sylvia Herbert 2016-9-12
+      
+      if numel(x) ~= obj.nx
+        error('Initial state does not have right dimension!');
+      end
+      
+      if ~iscolumn(x)
+        x = x';
+      end
+
+      if nargin < 2
+        T1Max = 5;
+      end
+      
+      if nargin < 3
+        T1Min = -T1Max;
+      end
+      
+      if nargin < 4
+        T2Max = 5;
+      end
+      
+      if nargin < 5
+        T2Min = -T2Max;
+      end
+      
+      if nargin < 6
+        m = 5;
+      end
+      
+      if nargin < 7
+        grav = 9.81;
+      end
+      
+      if nargin < 8
+        transDrag = 5;
+      end
+      
+      if nargin < 9
+        rotDrag = 5;
+      end
+      
+      if nargin < 10
+        Iyy = 5;
+      end
+      
+      if nargin < 11
+        l = 5;
+      end
+      
+      % Basic vehicle properties
+      obj.pdim = [1 3]; % Position dimensions
+      obj.hdim = 5;   % Heading dimensions
+      obj.nx = length(dims);
+      obj.nu = 2;  
+      
+      obj.x = x;
+      obj.xhist = obj.x;
+      obj.dims = dims;
+      
+      obj.T1Max = T1Max;
+      obj.T1Min = T1Min;
+      obj.T2Max = T2Max;
+      obj.T2Min = T2Min;
+      obj.m = m;
+      obj.grav = grav;
+      obj.transDrag = transDrag;
+      obj.rotDrag = rotDrag;
+      obj.Iyy = Iyy;
+      obj.l = l;
+    end
+    
+  end % end methods
+end % end classdef
---
+++ dynSys/@Quad6D/dynamics.m
@@ -0,0 +1,63 @@
+function dx = dynamics(obj, ~, x, T1, T2, ~, dims)
+% Dynamics of the Dubins Car
+%    \dot{x}_1 = v * cos(x_3)
+%    \dot{x}_2 = v * sin(x_3)
+%    \dot{x}_3 = w
+%   Control: u = w;
+%
+% Mo Chen, 2016-06-08
+
+if nargin < 6
+  dims = 1:obj.nx;
+end
+
+if iscell(x)
+  dx = cell(length(dims), 1);
+  
+  for i = 1:length(dims)
+    dx{i} = dynamics_cell_helper(obj, x, T1, T2, dims, dims(i));
+  end
+else
+  dx = zeros(obj.nx, 1);
+  
+  dx(1) = x(2);
+  dx(2) = (-(1/obj.m)*obj.transDrag*x(2))+...
+          ((-1/obj.m)*sin(x(5))*T1)+...
+          ((-1/obj.m)*sin(x(5))*T2);
+  dx(3) = x(4);
+  dx(4) = (-1/obj.m)*(obj.m*obj.grav + obj.transDrag*x(4)) +...
+          ((1/obj.m)*cos(x(5))*T1)+...
+          ((1/obj.m)*cos(x(5))*T2);
+  dx(5) = x(6);
+  dx(6) = ((-1/obj.Iyy)*obj.rotDrag*x(6))+...
+          ((-obj.l/obj.Iyy)*T1)+...
+          ((obj.l/obj.Iyy)*T2);
+  
+end
+end
+
+function dx = dynamics_cell_helper(obj, x, T1, T2, dims, dim)
+
+switch dim
+  case 1
+    dx = x{dims==2};
+  case 2
+    dx = (-(1/obj.m)*obj.transDrag*x{dims==2})+...
+          ((-1/obj.m)*sin(x{dims==5})*T1)+...
+          ((-1/obj.m)*sin(x{dims==5})*T2);
+  case 3
+    dx = x{dims==4};
+  case 4
+    dx = (-1/obj.m)*(obj.m*obj.grav + obj.transDrag*x{dims==4}) +...
+      ((1/obj.m)*cos(x{dims==5})*T1)+...
+      ((1/obj.m)*cos(x{dims==5})*T2);
+  case 5
+    dx = x{dims==6};
+  case 6
+    dx = ((-1/obj.Iyy)*obj.rotDrag*x{dims==6})+...
+      ((-obj.l/obj.Iyy)*T1)+...
+      ((obj.l/obj.Iyy)*T2);
+  otherwise
+    error('Only dimension 1-6 are defined for dynamics of Quad6D!')
+end
+end
